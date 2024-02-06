fbb8656b3172ccee4049952a602bf542f7df449f
mmaury2727 <120041871+mmaury2727@users.noreply.github.com>
2024-02-06 12:03:40 +0100

    gestion collision avec la clope

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag == "Clope")
+        {
+            print(" AHHHHHHH JE SUIS TOUCHE ");
+        }
     }
 }
