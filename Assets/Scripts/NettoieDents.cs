98648f9606b288dc834e184933d19d7eabd4d502
mmaury2727 <120041871+mmaury2727@users.noreply.github.com>
2024-02-09 14:26:45 +0100

    proto lavage de dents fini

--- Assets/Scripts/NettoieDents.cs
+++ Assets/Scripts/NettoieDents.cs
@@ -7,19 +7,76 @@ public class NettoieDents : MonoBehaviour
 {
     public Text m_Text;
 
+    private ImageAvecCoordonnees[] tabImgWithCoord;
+
+    [SerializeField] Image[] tabImages;
+
+
+    private void Start()
+    {
+        tabImgWithCoord = new ImageAvecCoordonnees[tabImages.Length];
+        int counter = 0;
+        foreach (Image img in tabImages){
+            tabImgWithCoord[counter] = new ImageAvecCoordonnees(img, CalcPosition(img)[0], CalcPosition(img)[1]);
+            print(tabImgWithCoord[counter].X + " "+ tabImgWithCoord[counter].Y);
+            counter++;
+        }
+    }
+
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            // Update the Text on the screen depending on current position of the touch each frame
             m_Text.text = "Touch Position : " + touch.position;
+
+            foreach (Image img in tabImages)
+            {
+                if ((img.transform.position.x - 30 <= touch.position.x && touch.position.x <= img.transform.position.x + 30) && (img.transform.position.x - 30 <= touch.position.x && touch.position.x <= img.transform.position.x + 30))
+                {
+                    img.gameObject.SetActive(false);
+                }
+            }
+
         }
         else
         {
             m_Text.text = "No touch contacts";
         }
+    }
+
+    float[] CalcPosition(Image img)
+    {
+        float[] positionImg = new float[2];
+
+        positionImg[0] = img.transform.position.x + 960;
+        positionImg[1] = img.transform.position.y + 540;
+
+        return positionImg;
+    }
+
+    bool IsOnDirt(float xImg, float yImg, float xPointer, float yPointer)
+    {
+        if ((xPointer - 10 <= xImg || xImg <= xPointer + 10) && (yPointer - 10 <= yImg || yImg <= yPointer + 10))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class ImageAvecCoordonnees
+{
+    public Image Image { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public ImageAvecCoordonnees(Image image, float x, float y)
+    {
+        Image = image;
+        X = x;
+        Y = y;
     }
 }
 
