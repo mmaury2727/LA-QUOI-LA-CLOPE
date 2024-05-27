7a8495774f4498ecde0297cd2f6f58b1a4956423
Loïs L. <36632834+LifestreamVII@users.noreply.github.com>
2024-05-27 22:42:49 +0200

    Fixed Tempete

--- Assets/Game/1. Scripts/TempeteDeClope/PlayerController.cs
+++ Assets/Game/1. Scripts/TempeteDeClope/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private float speed = 10.0f;
     private float jumpForce = 6f;
     private float xBound = 3.6f;
+    private float xMin;
+    private float xMax;
+    private Rect camsize;
     private bool isOnGround = true;
     private Animator animator;
 
@@ -20,6 +24,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         manager = GameObject.Find("Manager").GetComponent<Manager>();
         animator = GetComponent<Animator>();
+        camsize = GameObject.Find("Main Camera").GetComponent<Camera>().pixelRect;
     }
 
     void Update()
@@ -31,35 +36,44 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    float convertPosition(float coordX)
+    {
+        print(coordX);
+        return coordX * 7 / camsize.width; // -3.5 et 3.5 (-xBound, xBound)
+    }
+
     float GetTouchXPosition()
     {
         float touchPosX = Input.GetTouch(0).position.x;
-        Vector3 touchPosWorld = Camera.main.ScreenToWorldPoint(new Vector3(touchPosX, Mathf.Abs(transform.position.y - Camera.main.transform.position.y), Mathf.Abs(transform.position.z - Camera.main.transform.position.z)));
-        return touchPosWorld.x;
+        print("posPoubelleX = " + transform.position.x);
+        //Vector3 touchPosWorld = Camera.main.ScreenToWorldPoint(new Vector3(touchPosX, Mathf.Abs(transform.position.y - Camera.main.transform.position.y), Mathf.Abs(transform.position.z - Camera.main.transform.position.z)));
+        return touchPosX;
     }
 
     void MovePlayer()
     {
-        if (transform.position.x < -xBound)
-        {
-            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
-        } 
-        else if (transform.position.x > xBound)
-        {
-            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
-        } 
-        else if (Input.touchCount > 0)
+        //if (transform.position.x < -xBound)
+        //{
+        //    transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        //} 
+        //else if (transform.position.x > xBound)
+        //{
+        //    transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        //} 
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            Debug.Log(touch.position);
+            float pourcent = ((touch.position.x/camsize.width) * 100);
+
+              
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    transform.position = new Vector3(GetTouchXPosition(), transform.position.y, transform.position.z);
+                    transform.position = new Vector3(convertPosition(GetTouchXPosition()) - 2.5f, transform.position.y, transform.position.z);
                     break;
                 case TouchPhase.Moved:
                 case TouchPhase.Stationary:
-                    transform.position = new Vector3(GetTouchXPosition(), transform.position.y, transform.position.z);
+                    transform.position = new Vector3(convertPosition(GetTouchXPosition()) - 2.5f, transform.position.y, transform.position.z);
                     break;
                 case TouchPhase.Ended:
                     playerRb.velocity = Vector2.zero;
